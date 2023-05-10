87101f26d6998da515832e51204f5696cc101dd0
yoshikawa-river <yuttta0411@gmail.com>
2023-05-11 08:07:47 +0900

    エラー表示の修正

--- src/inputReward.js
+++ src/inputReward.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
 import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import './css/input.css';
-
-const ariaLabel = { 'aria-label': 'description' };
+import './css/main.css';
+import { FormHelperText } from '@mui/material';
 
 export const InputReward = ({ reward, onChange, placeholder, startBtnState }) => {
     const [errors, setErrors] = useState('');
@@ -40,22 +32,39 @@ export const InputReward = ({ reward, onChange, placeholder, startBtnState }) =>
                 noValidate
                 autoComplete="off"
                 >
-                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-                    <OutlinedInput
-                        placeholder={placeholder}
-                        value={reward === 0 ? '' : reward}
-                        onChange={(e) => onChangeHandler(e.target.value)}
-                        endAdornment={<InputAdornment position="end">えん</InputAdornment>}
-                        inputProps={{
-                            'aria-label': 'yen',
-                        }}
-                        disabled={startBtnState}
-                    />
-                </FormControl>
+                {errors.length === 0 ?
+                    (
+                        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <Input
+                            value={reward === 0 ? '' : reward}
+                            onChange={(e) => onChangeHandler(e.target.value)}
+                            endAdornment={<InputAdornment position="end">えん</InputAdornment>}
+                            inputProps={{
+                                'aria-label': 'yen',
+                                style: { 'fontSize': '2rem' }
+                            }}
+                            disabled={startBtnState}
+                        />
+                        </FormControl>
+                    )
+                    :
+                    (
+                        <FormControl error sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <Input
+                            value={reward === 0 ? '' : reward}
+                            onChange={(e) => onChangeHandler(e.target.value)}
+                            endAdornment={<InputAdornment position="end">えん</InputAdornment>}
+                            inputProps={{
+                                'aria-label': 'yen',
+                                style: { 'fontSize': '2rem' }
+                            }}
+                            disabled={startBtnState}
+                        />
+                        <FormHelperText className='reward-error' >{errors}</FormHelperText>
+                        </FormControl>
+                    )
+                }
             </Box>
-            <div>
-                {errors.length !== 0 && <p>{errors}</p>}
-            </div>
         </>
     );
 };
