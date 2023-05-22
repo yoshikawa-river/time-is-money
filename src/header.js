b258fffa43856b9974e09675fce86e386004f97c
yoshikawa-river <yuttta0411@gmail.com>
2023-05-23 01:54:34 +0900

    ヘッダー作成

---
+++ src/header.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import './css/header.css';
+
+export default function Header() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box sx={{ width: '50%' }} className="header-container" >
+        <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="secondary tabs example"
+        >
+            <Tab
+                style={{ 'fontSize': '2rem', 'width': '100%', 'fontWeight': 'bold', 'margin-left':'auto' }}
+                value={0}
+                label="ほうしゅうがた"
+            />
+            <Tab
+                style={{ 'fontSize': '2rem', 'width': '100%', 'fontWeight': 'bold', 'margin-right':'auto'}}
+                value={1}
+                label="じきゅうがた"
+            />
+        </Tabs>
+        </Box>
+    );
+}
